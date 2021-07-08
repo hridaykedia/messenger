8e5f5c9ee956b4fff4ac47927f07a8b323d0d3f2
Hriday Kedia <hridaykedia@Hridays-MacBook-Air.local>
2021-07-08 21:11:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { FormControl, Input, IconButton } from '@material-ui/core';
+import Message from './Message';
 import './App.css';
+import db from './firebase';
+import firebase from 'firebase';
+import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    setUsername(prompt('please enter a username'));
+  }, []);
+
+  useEffect(() => {
+    db.collection('messages')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
+      });
+  }, []);
+
+  const sendMessages = (e) => {
+    e.preventDefault();
+    db.collection('messages').add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <h1>Messenger</h1>
+        <form className='app__form'>
+          <FormControl className='app__formControl'>
+            <Input
+              className='app__input'
+              placeholder='Enter a message'
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <IconButton
+              className='app__iconButton'
+              disabled={!input}
+              variant='contained'
+              color='primary'
+              type='submit'
+              onClick={sendMessages}
+            >
+              <SendIcon />
+            </IconButton>
+          </FormControl>
+        </form>
+        <FlipMove>
+          {messages.map(({ id, message }) => (
+            <Message key={id} username={username} message={message} />
+          ))}
+        </FlipMove>
+      </div>
+    </>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: '<API_KEY>',
+  authDomain: '<PROJECT_ID>.firebaseapp.com',
+  databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
+  projectId: '<PROJECT_ID>',
+  storageBucket: '<BUCKET>.appspot.com',
+  messagingSenderId: '<SENDER_ID>',
+  appId: '<APP_ID>',
+  measurementId: '<MEASURMENT_ID>',
+});
+
+const db = firebaseApp.firestore();
+export default db;
